f3c07da420fdac91b0f59337de78625e2e0f2b34
wam <wam@onesty-tech.de>
2021-01-22 18:08:11 +0100

    Add route methods for bookStore api
    - Add fill database method
    - Add clean database method

--- server/routes/Book.route.js
+++ server/routes/Book.route.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const expressAsyncHandler = require("express-async-handler");
 const bookController = require("../controllers/Book.controller");
+const {Book} = require("../models/Book.model");
+const {booksList} = require("../../database/booksList");
 
 const bookRouter = express.Router();
+
+bookRouter.get("/seed", expressAsyncHandler(async (req, res) => {
+        const seed = await Book.insertMany(booksList);
+        res.status(201).send(seed);
+    }
+));
+
+bookRouter.get("/clear", expressAsyncHandler(async (req, res) => {
+        const response = await Book.deleteMany({});
+        if (response) {
+            res.send({message: "All books were deleted!", response: response});
+        } else {
+            //todo: failed deletion msg
+        }
+    }
+));
 
 bookRouter.get("/stock", expressAsyncHandler(bookController.getAvailableBooks));
 bookRouter.put("/:id", expressAsyncHandler(bookController.borrowBook));
