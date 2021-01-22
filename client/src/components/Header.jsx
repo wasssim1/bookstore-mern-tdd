4434935a5aeedf4a6fdc8b34a45a8e561b758586
wam <wam@onesty-tech.de>
2021-01-22 20:33:58 +0100

    Add fill database button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Axios from 'axios';
+import {useSelector} from "react-redux";
 
 function Header() {
+    const books = useSelector(state => state.books);
+    const {booksList} = books;
+
+    const onClickFillData = () => {
+        alert('hey')
+        Axios.get("/api/books/seed")
+            .then(() => console.log("Data filled!"))
+            .then(err => console.error(err));
+    };
+
     return (
         <div className="app_header">
+            {!booksList?.length > 0 &&
+            <button onClick={() => onClickFillData()}>Fill data</button>
+            }
             <h1>Books Store</h1>
         </div>
     );
